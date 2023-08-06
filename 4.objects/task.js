eb3fb516da363c9ff4caf74e17178990a8b097e3
Viktoriya Buzmakova <vik-po4ta@mail.ru>
2023-08-06 20:52:19 +0300

    ready-4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age, marks) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+const student1 = new Student('Маша', 'ж', 22);
+const student2 = new Student('Женя', 'ж', 23);
+const student3 = new Student('Саша', 'м', 21);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+  if (this.excluded) {
+    return; 
+}
+    if(this.marks){
+        this.marks.push(...marks);
+    }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks.length || !this.marks || this.excluded){
+    return 0;
+  } else{
+ return this.marks.reduce((acc, item) => (acc += item), 0) / this.marks.length;
+}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
+
