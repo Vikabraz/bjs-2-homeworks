27353a1575c312869463e79934c0e1ba2d5ba4f3
Viktoriya Buzmakova <vik-po4ta@mail.ru>
2023-08-15 21:54:44 +0300

    dz - classes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -14,9 +14,7 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.excluded) {
-    return; 
-}
+
     if(this.marks){
         this.marks.push(...marks);
     }
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿function parseCount(num) {
+    if (Number.parseFloat(num)){
+        return Number.parseFloat(num)
+    }
+    else{
+        throw new Error ('Невалидное значение');
+    }
+
+}
+
+function validateCount(num){
+
+try{
+    let result = parseCount(num);
+    if (result) {
+        return result;
+}
+} catch(Error) {
+    return Error;
+}
+}
+
+// задача 2
+
+// class Triangle{
+//     constructor(sideF, sideSec, sideTh){
+//     this.sideF = sideF;
+//     this.sideSec = sideSec;
+//     this.sideTh = sideTh;
+//     this._perimeter = null;
+//     this._area = null;
+//     }
+
+
+    
+//     get perimeter() {
+//         this._perimeter = (this.sideF + this.sideSec + this.sideTh);
+//             return this._perimeter;
+//     }
+
+//     get area () {
+//       this._area = +(Math.sqrt(this._perimeter / 2 * (this._perimeter / 2 - this.sideF) * (this._perimeter / 2 - this.sideSec) * (this._perimeter / 2 - this.sideTh))).toFixed(3);
+//       return this._area;
+//     }
+    
+// }
+
+// if (((sideF + sideSec) < sideTh) || ((sideF + sideTh) < sideSec) || ((sideSec + sideTh) < sideF)) {
+//     throw new Error('Треугольник с такими сторонами не существует');
+// }
+
+
+
+//  function getTriangle(sideF, sideSec, sideTh);
+
+
+
+
+
