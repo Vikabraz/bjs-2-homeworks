205adddec7c9f5d0840aba831e6ee8d66681a3e7
Viktoriya Buzmakova <vik-po4ta@mail.ru>
2023-08-20 14:29:39 +0300

    ready -6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -22,37 +22,59 @@ try{
 
 // задача 2
 
-// class Triangle{
-//     constructor(sideF, sideSec, sideTh){
-//     this.sideF = sideF;
-//     this.sideSec = sideSec;
-//     this.sideTh = sideTh;
-//     this._perimeter = null;
-//     this._area = null;
-//     }
+class Triangle{
+    constructor(sideF, sideSec, sideTh){
+        
+        if (sideF + sideSec <= sideTh || sideF + sideTh <= sideSec || sideSec + sideTh <= sideF) {
+    throw new Error('Треугольник с такими сторонами не существует');
+}
+
+    this.sideF = sideF;
+    this.sideSec = sideSec;
+    this.sideTh = sideTh;
+  
+    }
 
 
     
-//     get perimeter() {
-//         this._perimeter = (this.sideF + this.sideSec + this.sideTh);
-//             return this._perimeter;
-//     }
+    get perimeter() {
+        const perimeter = this.sideF + this.sideSec + this.sideTh;
+            return perimeter;
+    }
 
-//     get area () {
-//       this._area = +(Math.sqrt(this._perimeter / 2 * (this._perimeter / 2 - this.sideF) * (this._perimeter / 2 - this.sideSec) * (this._perimeter / 2 - this.sideTh))).toFixed(3);
-//       return this._area;
-//     }
+    get area () {
+      const area = +(Math.sqrt (this.perimeter / 2 * (this.perimeter / 2 - this.sideF) * (this.perimeter / 2 - this.sideSec) * (this.perimeter / 2 - this.sideTh))).toFixed(3);
+      return area;
+    }
     
-// }
-
-// if (((sideF + sideSec) < sideTh) || ((sideF + sideTh) < sideSec) || ((sideSec + sideTh) < sideF)) {
-//     throw new Error('Треугольник с такими сторонами не существует');
-// }
+}
 
 
 
-//  function getTriangle(sideF, sideSec, sideTh);
 
+ function getTriangle(sideF, sideSec, sideTh){
+    try{
+    const triangle1 = new Triangle(sideF, sideSec, sideTh);
+    return triangle1;
+    } catch(error){
+
+        return {
+            get perimeter(){
+             return 'Ошибка! Треугольник не существует';
+        },
+            get area(){
+              return  'Ошибка! Треугольник не существует';
+            } 
+        };    
+    }
+    }
+    
+    
+
+
+   
+
+ 
 
 
 
