0e03ae50d4d56fad6bf3caa7942f771c45831e25
Viktoriya Buzmakova <vik-po4ta@mail.ru>
2023-08-27 19:46:31 +0300

    7 ready

---
+++ 7.async/task.js
@@ -0,0 +1,63 @@
+class AlarmClock{
+    constructor(){
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback){
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+
+        if(this.alarmCollection.includes(time)){
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({
+            callback: callback,
+            time: time,
+            canCall: true
+        });
+}
+    removeClock(time){
+        this.alarmCollection.filter(item => item.time !== time);
+
+    }
+    getCurrentFormattedTime(){
+        const timeNow = new Date;
+        const hours = timeNow.getHours().toString().padStart(2,"0");
+        const minutes = timeNow.getMinutes().toString().padStart(2,"0");
+        return `${hours}:${minutes}`;
+    }
+    start(){
+        if(this.intervalId){
+            return;
+        }
+
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+        
+        this.alarmCollection.forEach(item =>{
+            if(item.time === currentTime && this.alarmCollection.canCall){
+                item.canCall = false;
+                item.callback();
+            }
+        });
+        }, 1000)
+    }
+    stop(){
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls(){
+        this.alarmCollection.forEach(item => item.canCall = true);
+    }
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = [];
+    }
+
+
+
+}
